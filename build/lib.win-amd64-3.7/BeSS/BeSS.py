d2c156611c921e091700ab3b0ca80020948bbfc7
Zlin_Hong <1006500074@qq.com>
2019-12-15 01:04:23 +0800

    update

--- build/lib.win-amd64-3.7/BeSS/BeSS.py
+++
@@ -1,8 +0,0 @@
-from BeSS.cbess import pywrap_bess_lm
-
-
-def bess_lm(X, y, T0, max_steps, beta, weights, normal = True):
-    result = pywrap_bess_lm(X, y, T0, max_steps, beta, weights, len(beta), T0, normal)
-    return result
-
-
