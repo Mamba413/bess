60bff2ac7dfdcff16abd7ef15535dfcf75a7d893
mamba413 <zhuj37@mail2.sysu.edu.cn>
2021-01-09 19:24:49 +0800

    update datasets

--- R/data-raw/DATASET.R
+++ R/data-raw/DATASET.R
@@ -14,14 +14,14 @@ duke <- cbind.data.frame("y" = duke_breast_cancer[, 1], duke)
 usethis::use_data(duke, duke, overwrite = TRUE)
 
 
-################## Trime 32 dataset ##################
-trime32 <- readRDS("F:\\pdas_l0l2\\bess\\data\\trim32.rds")
+################## Trim 32 dataset ##################
+trim32 <- readRDS("F:\\pdas_l0l2\\bess\\data\\trim32.rds")
 
-sis_fit <- SIS(as.matrix(trime32[["x"]]), as.vector(trime32[["y"]]),
+sis_fit <- SIS(as.matrix(trim32[["x"]]), as.vector(trim32[["y"]]),
                family = "gaussian", iter = FALSE, nsis = 500)
 
-trime32[["x"]] <- trime32[["x"]]
-trim32_x <- trime32[["x"]][, sis_fit[["sis.ix0"]]]
+trim32[["x"]] <- trim32[["x"]]
+trim32_x <- trim32[["x"]][, sis_fit[["sis.ix0"]]]
 
-trime32 <- cbind.data.frame("y" = trime32[["y"]], trim32_x)
-usethis::use_data(trime32, trime32, overwrite = TRUE)
+trim32 <- cbind.data.frame("y" = trim32[["y"]], trim32_x)
+usethis::use_data(trim32, trim32, overwrite = TRUE)
